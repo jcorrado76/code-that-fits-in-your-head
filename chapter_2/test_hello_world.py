94d0258b76a770cff0cb055e4365bc1b187d7e97
Joseph Corrado <josephcorrado7@gmail.com>
2023-05-18 15:18:44 -0400

    changed setup phase for pytest fixture

--- chapter_2/test_hello_world.py
+++ chapter_2/test_hello_world.py
@@ -1,10 +1,15 @@
+# Third Party Imports
+import pytest
 from fastapi.testclient import TestClient
 from hello_world import app
 
-client = TestClient(app)
+
+@pytest.fixture
+def test_client():
+    return TestClient(app)
 
 
-def test_get_hello_world():
-    response = client.get("/")
+def test_get_hello_world(test_client):
+    response = test_client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
